cfde7d35acad1e979f8845978ea302176170bc5a
marlonacademi <marlon.barbosa@focusit.com.br>
2024-10-18 01:05:56 -0300

    Table list payments

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import  { createContext } from 'react';
 import { Header } from './Components/Header';
 import { Routes } from './Routes';
-
 import './App.css';
 
 type PlanDataType = {
@@ -36,11 +35,21 @@ export const contextPlanData = createContext(PlanData);
 
 function App() {
 
+    const pathName = window.location.pathname
+
+    let ClassNameContainer = "max-w-7xl mx-auto py-2 px-4 tablet:px-4";
+
+    if (pathName === '/payments/juripass/1486fa01-8794-4bf0-a68a-cb47bab66032') {
+
+        ClassNameContainer = "w-full mx-auto py-2 px-4 tablet:px-4";
+
+    }
+
     return (
         <div className="App ">
             <Header />
             <contextPlanData.Provider value={PlanData}>
-                <section className="max-w-7xl mx-auto py-2 px-4 tablet:px-4">
+                <section className={ClassNameContainer}>
 
                     <Routes />
 
--- src/Pages/PaymentsList.tsx
+++ src/Pages/PaymentsList.tsx
@@ -1,20 +1,27 @@
 
-import { useCallback, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PageLoader from "../Components/PageLoader";
-import JSONPretty from 'react-json-pretty';
-import 'react-json-pretty/themes/monikai.css';
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+var payments: any = [];
 
 export const PaymentsList = () => {
-
-    const [reload, setReload] = useState(true);
-
-    var payments: any = [];
+    
+    const schema = z.object({
+        cpf: z.string()
+    });
 
     const [loading, setLoading] = useState(false);
     
     const [paymentsFiltered, setPaymentsFiltered] = useState<any>([]);
     
-    var JSONPrettyMon = require('react-json-pretty/dist/monikai');
+    type FormData = z.infer<typeof schema>;
+
+    const { register, handleSubmit } = useForm<FormData>({
+        resolver: zodResolver(schema),
+    });
 
     const loadPayment = (params:any) => {
 
@@ -45,8 +52,16 @@ export const PaymentsList = () => {
 
     const filterPayment = (params:any) => {
 
-        setPaymentsFiltered(payments.filter((payment:any) => {
+        setPaymentsFiltered(
+            payments.filter((payment:any) => {
                 let isFiltered = true;
+
+                if (params.cpf && params.cpf.length > 0){
+
+                    if (!payment.request.servicePlan.cpf.replace(/\D/g, '').includes(params.cpf)){
+                        isFiltered = false;
+                    }
+                }
 
                 if (isFiltered)
                     return true;
@@ -67,9 +82,39 @@ export const PaymentsList = () => {
 
     }, []);
 
+    async function handleSubmitFilter(data: FormData) {
+        filterPayment(data);
+    }
+
+    async function onErrors(e: any) {
+        
+
+    }
+
     return <>
 
         <PageLoader isShow={loading} />
+
+        <form
+            className="flex-col gap-4"
+            onSubmit={handleSubmit(handleSubmitFilter, onErrors)}
+        >
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8 text-left mb-4">
+                <div className="mt-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2">
+                        CPF
+                    </label>
+                    <div className="flex">
+                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cpf" type="text" placeholder="CPF" {...register("cpf")}></input>
+
+                        <button type="submit" className="text-center cursor-pointer text-white bg-juripass font-semibold py-2 px-6 hover:text-[#d9d9d9] transition duration-500 ease-in-out">
+                            Filtrar
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </form>
 
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
 
@@ -77,37 +122,145 @@ export const PaymentsList = () => {
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr>
                         <th scope="col" className="px-6 py-3">
+                            Nome
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            CPF
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Telefone
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Tipo usuário
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Grau parentesco
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            CPF Vinculado
+                        </th>
+                        <th scope="col" className="px-6 py-3">
                             Plano
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Valor
-                        </th>
-                        <th scope="col" className="px-6 py-3">
-                            Status
                         </th>
                         <th scope="col" className="px-6 py-3">
                             Data
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            Dados do plano
+                            Valor
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Cupon
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Forma de pagamento
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Status
                         </th>
                     </tr>
                 </thead>
                 <tbody>
 
                     {paymentsFiltered.map((payment:any) => {
+
+                        let className = payment.status === "approved" ? "bg-white border-b" : "bg-red-100 border-b";
                         return (
-                            <tr className="bg-white border-b " key={payment.id}>
-                                <td className="px-6 py-4">{payment.request.servicePlan.planName}</td>
-                                <td className="px-6 py-4">{payment.amount}</td>
-                                <td className="px-6 py-4">{payment.status}</td>
-                                <td className="px-6 py-4">{payment.created_at}</td>
-                                <td className="px-6 py-4">
-                                    <div className="w-full h-[20rem] overflow-auto">
-                                        <JSONPretty theme={JSONPrettyMon} id="json-pretty" data={payment.request.servicePlan} />
-                                    </div>
-                                </td>
-                            </tr>
+                            <>
+                                <tr className={className} key={payment.id}>
+
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.name}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.cpf}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.telefone}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.email}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        Principal
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.cpf}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.planName}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.created_at}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.planValue.toFixed(2)}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.coupon}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.request.servicePlan.payment_method_id}
+                                    </td>
+                                    <td scope="col" className="px-6 py-4">
+                                        {payment.status}
+                                    </td>
+                                </tr>
+                                { payment.request.servicePlan.dependentes.length > 0 && 
+                                    payment.request.servicePlan.dependentes.map((dependent:any) => {
+                                        return (
+                                            <>
+                                                <tr className="bg-white border-b " key={payment.id}>
+
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {dependent.name}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {dependent.cpf}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {dependent.telefone}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {dependent.email}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        Dependente
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {dependent.grau_parentesco}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        {payment.request.servicePlan.cpf}
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                       
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        
+                                                    </td>
+                                                    <td scope="col" className="px-6 py-4">
+                                                        
+                                                    </td>
+                                                </tr>
+                                            </>
+                                        )   
+                                })}
+                            </>
                         );
                     })}                   
                 </tbody>
